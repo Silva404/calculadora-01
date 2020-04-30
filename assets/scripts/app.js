0fc3b7f1f65d47523a512ca7f36c9a0b82845efc
Breno <brenosilva0252@gmail.com>
2020-04-30 17:42:31 -0300

    iniciando a calculadora

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = 0;
+
+currentResult = (defaultResult + 10) / 2 * 15 - 7;
+
+let calculationDescription = ` (${defaultResult} + 10) / 2 * 15 - 7`;
+
+outputResult(currentResult, calculationDescription);
