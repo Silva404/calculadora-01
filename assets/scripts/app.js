d45813d4f3248f4c32bb13d2cedd380b36e09060
Breno <brenosilva0252@gmail.com>
2020-04-30 19:18:22 -0300

    updatea

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0;
 let currentResult = 0;
 
+function add(num1, num2){
+  const result = num1 + num2;   
+  return result;
+}
+
+add(defaultResult, currentResult);
+add(1,2);
+
 currentResult = (defaultResult + 10) / 2 * 15 - 7;
 
 let calculationDescription = ` (${defaultResult} + 10) / 2 * 15 - 7`;
