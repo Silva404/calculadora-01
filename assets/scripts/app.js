f175fa42dddb3051407f1781594468691e4118b5
Breno <brenosilva0252@gmail.com>
2020-04-30 19:49:54 -0300

    vou tentar recriar

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,11 +6,8 @@ function add(num1, num2){
   return result;
 }
 
-add(defaultResult, currentResult);
-add(1,2);
+currentResult = add(4,2);
 
-currentResult = (defaultResult + 10) / 2 * 15 - 7;
-
-let calculationDescription = ` (${defaultResult} + 10) / 2 * 15 - 7`;
+// let calculationDescription = ` (${defaultResult} + 10) / 2 * 15 - 7`;
 
 outputResult(currentResult, calculationDescription);
