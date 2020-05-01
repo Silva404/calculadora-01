088c5f62c5c05794a64c95aa5be625608b982ba9
Breno <brenosilva0252@gmail.com>
2020-05-01 16:53:30 -0300

    agora entendi

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,11 @@
-const defaultResult = 0;
+const defaultResult = 0 ;
 let currentResult = defaultResult;
 
 function add(){
-  currentResult = currentResult + +userInput.value;
-  outputResult(currentResult, ' ');
+  const enteredNumber = parseInt(userInput.value)
+  const calcDescription = `${currentResult} + ${enteredNumber}`
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription )
 }
 
-addBtn.addEventListener('click', add);
+addBtn.addEventListener('click', add)
