0ec2ea59d85002c6827ccb74fb396ec02d0d9b7c
Breno <brenosilva0252@gmail.com>
2020-05-01 18:51:20 -0300

    entendendo essa chibata finalmente

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,19 +2,43 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function getUserNumberInput(){
-  return parseInt(userInput.value)
+  return parseInt(userInput.value);
 } 
 
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+function createAndWriteOutput(operator, resultbeforeCalc, calcNumber) {
+  const calcDescription = `${resultbeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
 function add() {
-  const enteredNumber = getUserNumberInput();  
-  const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;  
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  
+  currentResult = currentResult + enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
 } 
 
-addBtn.addEventListener('click', add)
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+} 
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+} 
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+} 
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
