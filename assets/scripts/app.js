d445627cc71f561003227287377812edf39d77f6
Breno <brenosilva0252@gmail.com>
2020-05-01 18:41:00 -0300

    refazendo do zero

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,9 @@
-const defaultResult = 0 ;
+const defaultResult = 0;
 let currentResult = defaultResult;
 
-function getUserNumberInput() {
+function getUserNumberInput(){
   return parseInt(userInput.value)
-}
+} 
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
@@ -13,35 +13,8 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
 function add() {
   const enteredNumber = getUserNumberInput();  
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult = currentResult + enteredNumber;  
   createAndWriteOutput('+', initialResult, enteredNumber);
-}
-
-function substract() {
-  const enteredNumber = getUserNumberInput();  
-  const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
-  createAndWriteOutput('-', initialResult, enteredNumber);
-}
-
-function multiply() {
-  const enteredNumber = getUserNumberInput();  
-  const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
-  createAndWriteOutput('*', initialResult, enteredNumber);
-}
-
-function divide() {
-  const enteredNumber = getUserNumberInput();  
-  const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
-  createAndWriteOutput('/', initialResult, enteredNumber);
-}
+} 
 
 addBtn.addEventListener('click', add)
-
-subtractBtn.addEventListener('click', substract)
-
-multiplyBtn.addEventListener('click', multiply)
-
-divideBtn.addEventListener('click', divide)
