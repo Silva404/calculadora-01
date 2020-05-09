65b197bdb736d4e108e8b694bddd21f5d879213b
Silva404 <brenosilva0252@gmail.com>
2020-05-09 20:40:46 -0300

    ate agora nada

---
+++ assets/scripts/script.js
@@ -0,0 +1,36 @@
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+
+keys.addEventListener("click", event => {
+  if (event.target.matches("button")) {};
+})
+
+// verificando os tipos de dados do botões clicados.
+
+const key = e.target;
+const action = key.dataset.action;
+
+if (!action) {
+  console.log("number key!");
+}
+
+if (
+  action === "add" ||
+  action === "subtract" ||
+  action === "multiply" ||
+  action === "divide" 
+) {
+  console.log("operator key!")  
+}
+
+if (action === 'decimal') {
+  console.log('decimal key!')
+}
+
+if (action === 'clear') {
+  console.log('clear key!')
+}
+
+if (action === 'calculate') {
+  console.log('equal key!')
+}
