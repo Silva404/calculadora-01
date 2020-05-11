af3312bc83cdbc3290930445916073baefe83b62
Silva404 <brenosilva0252@gmail.com>
2020-05-10 21:31:37 -0300

    recomeçando, to puto ja

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,8 @@
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
 
-keys.addEventListener("click", event => {
-  if (event.target.matches("button")) {};
+keys.addEventListener("click", e => {
+  if (e.target.matches("button")) {}
 })
 
 // verificando os tipos de dados do botões clicados.
