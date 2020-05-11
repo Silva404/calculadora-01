a13073f183c8cf4f03a3a9f437d2c41c6dbe6e21
Silva404 <brenosilva0252@gmail.com>
2020-05-10 22:10:39 -0300

    eae

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,54 +1,37 @@
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 ==0) {
-    console.log("FizzBuzz");
-  } else if (i % 5 == 0) {
-    console.log("Buzz"); 
-  } else if (i % 3 == 0) {
-    console.log("Fizz"); 
-  } else {
-    console.log(i);
-  }
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+
+keys.addEventListener("click", event => {
+  if (event.target.matches("button")) {};
+})
+
+// verificando os tipos de dados do botões clicados.
+
+const key = event.target;
+const action = key.dataset.action;
+
+if (!action) {
+  console.log("number key!");
 }
 
+if (
+  action === "add" ||
+  action === "subtract" ||
+  action === "multiply" ||
+  action === "divide" 
+) {
+  console.log("operator key!")  
+}
 
+if (action === 'decimal') {
+  console.log('decimal key!')
+}
 
+if (action === 'clear') {
+  console.log('clear key!')
+}
 
-
-
-// const calculator = document.querySelector(".calculator");
-// const keys = calculator.querySelector(".calculator__keys");
-
-// keys.addEventListener("click", event => {
-//   if (event.target.matches("button")) {};
-// })
-
-// // verificando os tipos de dados do botões clicados.
-
-// const key = e.target;
-// const action = key.dataset.action;
-
-// if (!action) {
-//   console.log("number key!");
-// }
-
-// if (
-//   action === "add" ||
-//   action === "subtract" ||
-//   action === "multiply" ||
-//   action === "divide" 
-// ) {
-//   console.log("operator key!")  
-// }
-
-// if (action === 'decimal') {
-//   console.log('decimal key!')
-// }
-
-// if (action === 'clear') {
-//   console.log('clear key!')
-// }
-
-// if (action === 'calculate') {
-//   console.log('equal key!')
-// }
+if (action === 'calculate') {
+  console.log('equal key!')
+}
 
