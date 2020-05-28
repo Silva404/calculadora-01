4bd4eed1e330d3a5aac871863df29a765502d920
Silva404 <brenosilva0252@gmail.com>
2020-05-28 17:31:25 -0300

    recomeçar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,37 +1,39 @@
-const calculator = document.querySelector(".calculator");
-const keys = calculator.querySelector(".calculator__keys");
+const calculator = document.getElementById(".calculator");
+const keys = document.getElementById(".calculator__keys");
 
-keys.addEventListener("click", event => {
-  if (event.target.matches("button")) {};
+keys.addEventListener("click", e => {
+  if (e.target.matches("button")) {
+    
+    if (!action) {
+      console.log("number key!");
+    }
+    
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide" 
+    ) {
+      console.log("operator key!")  
+    }
+    
+    if (action === 'decimal') {
+      console.log('decimal key!')
+    }
+    
+    if (action === 'clear') {
+      console.log('clear key!')
+    }
+    
+    if (action === 'calculate') {
+      console.log('equal key!')
+    }
+  };
 })
 
 // verificando os tipos de dados do botões clicados.
 
-const key = event.target;
+const key = e.target;
 const action = key.dataset.action;
 
-if (!action) {
-  console.log("number key!");
-}
-
-if (
-  action === "add" ||
-  action === "subtract" ||
-  action === "multiply" ||
-  action === "divide" 
-) {
-  console.log("operator key!")  
-}
-
-if (action === 'decimal') {
-  console.log('decimal key!')
-}
-
-if (action === 'clear') {
-  console.log('clear key!')
-}
-
-if (action === 'calculate') {
-  console.log('equal key!')
-}
 
